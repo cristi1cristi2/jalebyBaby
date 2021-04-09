f19ba0b15fb7c8a824384464dfa480ad116358e7
cristi.serbu17@gmail.com <cristi.serbu17@gmail.com>
2021-04-09 18:03:16 +0300

    online coop mode goes brrrr

--- chess/chess.cpp
+++ chess/chess.cpp
@@ -11,11 +11,17 @@
 #include <string>
 #include <fstream>
 #include <random>
+#include <mysql.h>
+#include <windows.h>
+
 
 int windowWidth = 1000, windowHeight = 800;
 int game = 0, nrBeri = 3, fast = 3, ok1 = 0, ok2 = 0, ok3 = 1;
 float constantFast = 5;
 int highscore = -1;
+int ID, ID2, GID;
+int punctele_mele = 0, visual_count = 0, puncte_coechipier = 0, bonus = 0;
+bool gidSet = false;
 std::random_device rd; // obtain a random number from hardware
 std::mt19937 gen(rd()); // seed the generator
 std::uniform_int_distribution<> distr(30, 970); // define the range
@@ -23,6 +29,7 @@ std::uniform_int_distribution<> distrp1(30, 450); // define the range
 std::uniform_int_distribution<> distrp2(550, 970); // define the range
 
 
+int delay = 0;
 
 Bere bere[100], bere2[100];
 
@@ -35,7 +42,7 @@ void initialize(Player* player1, Player* player2, bool pvp) {
 		(*player2).reset();
 		(*player2).setPosition(sf::Vector2f(999, 750));
 		nrBeri = 3;
-		fast = 1;
+		fast = 4;
 		ok1 = 0;
 		constantFast = 3;
 
@@ -51,7 +58,7 @@ void initialize(Player* player1, Player* player2, bool pvp) {
 	else {
 		game = 1;
 		nrBeri = 3;
-		fast = 1;
+		fast = 4;
 		ok1 = 0;
 		constantFast = 3;
 		(*player1).reset();
@@ -105,9 +112,55 @@ void idle(Player* player, float speed) {
 	pos.x += speed;
 	(*player).setPosition(pos);
 }
-
+int qstate;
 int main()
 {
+	//compile with this uncommented when you want to compile for a new version
+	ShowWindow(GetConsoleWindow(), SW_HIDE); //SW_RESTORE to bring back
+
+	MYSQL* conn;
+	MYSQL_ROW row;
+	MYSQL_RES* res;
+	conn = mysql_init(0);
+
+	conn = mysql_real_connect(conn, "sql11.freesqldatabase.com", "sql11403545", "ISkD7nsJW3", "sql11403545", 3306, NULL, 0);
+
+	if (conn) {
+		puts("Successful connection to database!");
+	}
+	else {
+		puts("Connection to database has failed!");
+		exit(2);
+	}
+
+	srand(time(NULL));
+	ID = rand() % 15;
+	GID = ID;
+	std::string query = "insert into game (id) values (" + std::to_string(ID) + ")";
+	const char* q = query.c_str();
+	qstate = mysql_query(conn, q);
+	query = "insert into player (id,game_id1, x, y) values (" + std::to_string(ID) + "," + std::to_string(ID) + ",495.4,44.444)";
+	q = query.c_str();
+	qstate = mysql_query(conn, q);
+
+
+	query = "SELECT * FROM player where id=" + std::to_string(ID);
+	std::cout << query << std::endl;
+
+	q = query.c_str();
+	qstate = mysql_query(conn, q);
+	res = mysql_store_result(conn);
+	row = mysql_fetch_row(res);
+	float x = std::stof(row[2]);
+	float y = std::stof(row[3]);
+	printf("%f %f\n", x, y);
+	x += 0.5;
+	y += 0.3;
+	query = "update player set x= " + std::to_string(x) + " , y= " + std::to_string(y) + " where id=" + std::to_string(ID) + ";";
+	std::cout << query << std::endl;
+	q = query.c_str();
+	qstate = mysql_query(conn, q);
+
 	//random
 
 	bool p2 = true, pvp = false;
@@ -157,7 +210,7 @@ int main()
 	music.setLoop(true);
 	if (!music.openFromFile("sounds\\music.wav"))
 		return -1; // error
-	music.setVolume(40);
+	music.setVolume(20);
 	music.play();
 
 	srand(time(NULL));
@@ -171,7 +224,7 @@ int main()
 		bere2[i].setTexture(cristi);
 	}
 
-	sf::Text score1, score2, start, players, versus;
+	sf::Text score1, score2, start, players, versus, room, plus1;
 	sf::Font font;
 	if (!font.loadFromFile("FontFile.ttf")) { printf("plm"); }
 
@@ -202,11 +255,23 @@ int main()
 	players.setCharacterSize(40);
 	players.setFillColor(sf::Color::Red);
 
+	room.setString("Room: " + std::to_string(GID));
+	room.setPosition(sf::Vector2f(400, 500));
+	room.setFont(font);
+	room.setCharacterSize(40);
+	room.setFillColor(sf::Color::Red);
+
 	versus.setString("PVP:OFF");
 	versus.setPosition(sf::Vector2f(500, 400));
 	versus.setFont(font);
 	versus.setCharacterSize(40);
 	versus.setFillColor(sf::Color::Red);
+
+	plus1.setString("+1");
+	plus1.setPosition(sf::Vector2f(400, 500));
+	plus1.setFont(font);
+	plus1.setCharacterSize(30);
+	plus1.setFillColor(sf::Color::Red);
 
 	//citim highscore din fisier si il stocam in highscore
 	std::fstream myfile("highscore.txt", std::ios_base::in);
@@ -231,7 +296,7 @@ int main()
 			fast++;
 		}
 		if (player1.getPoints() == 40 && ok1 == 3) {
-			fast+=2;
+			fast += 2;
 			ok1++;
 			constantFast += 1;
 			nrBeri++;
@@ -249,7 +314,7 @@ int main()
 		}
 		if (player1.getPoints() == 70 && ok1 == 6) {
 			nrBeri++;
-			fast+=2;
+			fast += 2;
 			ok1++;
 		}
 		if (player1.getPoints() == 80 && ok1 == 7) {
@@ -268,8 +333,12 @@ int main()
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == event.Closed)
+			if (event.type == event.Closed) {
 				window.close();
+				query = "delete from player where id=" + std::to_string(ID);
+				q = query.c_str();
+				mysql_query(conn, q);
+			}
 
 			if (event.type == sf::Event::MouseButtonPressed && game == 0) {
 				if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
@@ -313,27 +382,88 @@ int main()
 				if (event.key.code == sf::Keyboard::Escape) { game = 0; music.play(); }
 				if (event.key.code == sf::Keyboard::P && game != 1) { p2 = false; }
 				if (event.key.code == sf::Keyboard::O && game != 1) { p2 = true; }
+				if (event.key.code == sf::Keyboard::Num1 && game == 0) { GID++; }
+				if (event.key.code == sf::Keyboard::Num2 && game == 0) { GID--; }
 			}
 		}
 
 		if (game == 1) {
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-				if (player2.getPosition().x > 0)
-					moveLeft(&player2);
-			}
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-				if (player2.getPosition().x < 1000)
-					moveRight(&player2);
+			if (GID != ID && gidSet == false && p2) {
+				query = "update player set game_id1= " + std::to_string(GID) + " where id=" + std::to_string(ID) + ";";
+				//std::cout << query << std::endl;
+				q = query.c_str();
+				qstate = mysql_query(conn, q);
+				gidSet = true;
 			}
 
+			if (ID2 == 0 && p2) {
+				std::string query = "SELECT id FROM player WHERE game_id1=" + std::to_string(GID);
+				const char* q = query.c_str();
+				qstate = mysql_query(conn, q);
+				if (!qstate)
+				{
+					res = mysql_store_result(conn);
+					while (row = mysql_fetch_row(res))
+					{
+						int aux = std::stoi(row[0]);
+						if (aux != ID)
+							ID2 = aux;
+					}
+				}
+				else
+				{
+					std::cout << "Query failed: " << mysql_error(conn) << std::endl;
+				}
+			}
+			///partea de miscare
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
 				if (player1.getPosition().x > 0)
 					moveLeft(&player1);
+				delay++;
+				if (p2 && delay == 2) {
+					delay = 0;
+					query = "update player set x= " + std::to_string(player1.getPosition().x) + " , y= " + std::to_string(player1.getPosition().y) + " where id=" + std::to_string(ID) + ";";
+					//std::cout << query << std::endl;
+					q = query.c_str();
+					qstate = mysql_query(conn, q);
+					//delay = 0;
+				}
 			}
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
 				if (player1.getPosition().x < 1000)
 					moveRight(&player1);
+				delay++;
+				if (p2 && delay == 2) {
+					delay = 0;
+					query = "update player set x= " + std::to_string(player1.getPosition().x) + " , y= " + std::to_string(player1.getPosition().y) + " where id=" + std::to_string(ID) + ";";
+					//std::cout << query << std::endl;
+					q = query.c_str();
+					qstate = mysql_query(conn, q);
+					//delay = 0;
+				}
 			}
+			if (ID2 && p2) {
+				query = "SELECT * FROM player WHERE id=" + std::to_string(ID2);
+				q = query.c_str();
+				mysql_query(conn, q);
+				res = mysql_store_result(conn);
+				row = mysql_fetch_row(res);
+				if (row == nullptr)
+				{
+					query = "update player set game_id1= " + std::to_string(ID) + " where id=" + std::to_string(ID) + ";";
+					//std::cout << query << std::endl;
+					q = query.c_str();
+					qstate = mysql_query(conn, q);
+					gidSet = false;
+					game = 0;
+					GID = ID;
+					ID2 = 0;
+				}
+				else
+					player2.setPosition(sf::Vector2f(std::stof(row[2]), std::stof(row[3])));
+			}
+			//player2.setPosition(sf::Vector2f(std::stoi(row[0]), std::stoi(row[1])));
+
 		}
 		if (game == 3) {
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
@@ -417,6 +547,10 @@ int main()
 			window.draw(start);
 			window.draw(players);
 			window.draw(versus);
+			room.setString("Room: " + std::to_string(GID));
+			if (p2)
+				window.draw(room);
+
 		}
 		else
 			if (game == 1) {
@@ -444,6 +578,11 @@ int main()
 							player1.setPoints(player1.getPoints() + 1);
 							bere[i].setPosition(std::pair<float, float>(distr(gen), -100 - rand() % 500));
 							bere[i].setVelocity((float)(rand() % fast) + constantFast);
+
+							puncte_coechipier++;
+							query = "update player set puncte= " + std::to_string(puncte_coechipier) + " where id=" + std::to_string(ID2) + ";";
+							q = query.c_str();
+							qstate = mysql_query(conn, q);
 
 						}
 					}
@@ -517,9 +656,61 @@ int main()
 						game = 2;
 					}
 				}
+				else if (game == 2 && p2) {
+					query = "SELECT * FROM player WHERE id=" + std::to_string(ID2);
+					q = query.c_str();
+					mysql_query(conn, q);
+					res = mysql_store_result(conn);
+					row = mysql_fetch_row(res);
+					if (row == nullptr)
+					{
+						query = "update player set game_id1= " + std::to_string(ID) + " where id=" + std::to_string(ID) + ";";
+						//std::cout << query << std::endl;
+						q = query.c_str();
+						qstate = mysql_query(conn, q);
+						gidSet = false;
+						game = 0;
+						GID = ID;
+						ID2 = 0;
+					}
+					else
+						player2.setPosition(sf::Vector2f(std::stof(row[2]), std::stof(row[3])));
+					window.draw(player2.getForma());
+				}
 		window.draw(player1.getForma());
 		if (p2) {
 			window.draw(player2.getForma());
+			query = "SELECT puncte FROM player where id=" + std::to_string(ID);
+			q = query.c_str();
+			qstate = mysql_query(conn, q);
+			res = mysql_store_result(conn);
+			row = mysql_fetch_row(res);
+			if (row != nullptr) {
+				int aux = std::stoi(row[0]);
+				if (aux > punctele_mele) {
+					punctele_mele = aux;
+
+					if (visual_count > 0) {
+						bonus++;
+						plus1.setString("+" + std::to_string(bonus));
+					}
+					else
+					{
+						bonus = 1;
+						plus1.setString("+" + std::to_string(bonus));
+					}
+					visual_count += 40;
+					sf::Vector2f a = player1.getPosition();
+					a.x -= 60;
+					plus1.setPosition(a);
+
+				}
+			}
+			if (visual_count > 0) {
+				visual_count--;
+				window.draw(plus1);
+			}
+			else bonus = 0;
 		}
 
 		if (game == 3 || (game == 2 && pvp)) {
@@ -530,7 +721,7 @@ int main()
 		score1.setString("lives: " + std::to_string(player1.getLives()) + "\npoints: " + std::to_string(player1.getPoints()) + "\nHIGHSCORE: " + std::to_string(highscore));
 		window.draw(score1);
 		window.display();
-		//printf("%d\n", points);
+		//printf("%d %d\n", ID, ID2);
 
 	}
 }
